0b5be037f265a0d10455ea10e7f6a46f6c958520
992613643 <992613643@qq.com>
2018-12-03 18:12:41 +0800

    qwe

--- source/pages/goods/goods.js
+++ source/pages/goods/goods.js
@@ -10,14 +10,31 @@ class Content extends AppBase {
   onLoad(options) {
     this.Base.Page = this;
     //options.id=5;
+    this.Base.needauth=false;
     super.onLoad(options);
+   
   }
   onMyShow() {
     var that = this;
+    this.Base.setMyData({
+      num: 1,
+      price: 21,
+
+    })
+  }
+  jia(){
+console.log(123);
+
+  }
+  jian() {
+    console.log(456);
+
   }
 }
 var content = new Content();
 var body = content.generateBodyJson();
 body.onLoad = content.onLoad;
 body.onMyShow = content.onMyShow;
+body.jia=content.jia;
+body.jian=content.jian;
 Page(body)
