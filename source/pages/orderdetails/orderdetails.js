61c2af980ae95db26afc165856fcf1d8682a517b
992613643 <992613643@qq.com>
2018-12-04 14:12:25 +0800

    qwe

--- source/pages/orderdetails/orderdetails.js
+++ source/pages/orderdetails/orderdetails.js
@@ -11,37 +11,13 @@ class Content extends AppBase {
     this.Base.Page = this;
     //options.id=5;
     super.onLoad(options);
-    this.Base.setMyData({ num: 1 ,xuanzhon:1});
   }
   onMyShow() {
     var that = this;
   }
-  jian() {
-    var num = this.Base.getMyData().num;
-    if (num > 1) {
-      num--;
-      this.Base.setMyData({ num });
-    }
-  }
-  jia() {
-    var num = this.Base.getMyData().num;
-    if (num < 99) {
-      num++;
-      this.Base.setMyData({ num });
-    }
-  }
-  changexz(e) {
-    this.Base.setMyData({
-      xuanzhon: e.currentTarget.id
-    });
-  }
-
 }
 var content = new Content();
 var body = content.generateBodyJson();
 body.onLoad = content.onLoad;
 body.onMyShow = content.onMyShow;
-body.jia = content.jia;
-body.jian = content.jian;
-body.changexz = content.changexz;
 Page(body)
